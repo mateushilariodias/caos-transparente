853504ca590d974710596b868257deb018824dd6
mateushilariodias <mateusilariodias@gmail.com>
2023-09-09 10:09:05 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/processa_formulario', (req, res) => {
+    const formData = req.body;
+
+    // Ler o arquivo JSON atual, atualizar com os novos dados e salvar de volta
+    fs.readFile('submissions.json', (err, data) => {
+        if (err && err.code === 'ENOENT') {
+            // Se o arquivo não existir, inicializa com um array vazio
+            return fs.writeFile('submissions.json', JSON.stringify([formData], null, 2), error => {
+                if (error) throw error;
+                res.send('Dados salvos com sucesso!');
+            });
+        } else if (err) {
+            throw err;
+        }
+
+        // Se o arquivo existir, adicione a nova submissão
+        const json = JSON.parse(data.toString());
+        json.push(formData);
+
+        fs.writeFile('submissions.json', JSON.stringify(json, null, 2), error => {
+            if (error) throw error;
+            res.send('Dados salvos com sucesso!');
+        });
+    });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
